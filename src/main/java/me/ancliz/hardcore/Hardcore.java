94cf6b0dabc4e6f75ad8d5b429539c1f72befc6b
Ancliz <ancliz@ancliz.me>
2025-02-17 16:29:43 +1100

    Initial commit

---
+++ src/main/java/me/ancliz/hardcore/Hardcore.java
@@ -0,0 +1,17 @@
+package me.ancliz.hardcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hardcore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
